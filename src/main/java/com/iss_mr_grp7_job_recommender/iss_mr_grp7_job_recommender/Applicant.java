cfa952c5ba644c4090dfd0db030734f0182592d7
wbadmin <>
2020-03-25 18:23:12 +0800

    {/src/main/java/com/iss_mr_grp7_job_recommender/iss_mr_grp7_job_recommender/Applicant.java}

--- src/main/java/com/iss_mr_grp7_job_recommender/iss_mr_grp7_job_recommender/Applicant.java
+++ src/main/java/com/iss_mr_grp7_job_recommender/iss_mr_grp7_job_recommender/Applicant.java
@@ -19,9 +19,6 @@ public class Applicant implements java.io.Serializable {
 	@org.kie.api.definition.type.Label("Zip Code")
 	private java.lang.Integer zip_code;
 
-	@org.kie.api.definition.type.Label(value = "Error and Cause")
-	private java.lang.String error;
-
 	public Applicant() {
 	}
 
@@ -65,24 +62,14 @@ public class Applicant implements java.io.Serializable {
 		this.zip_code = zip_code;
 	}
 
-	public java.lang.String getError() {
-		return this.error;
-	}
-
-	public void setError(java.lang.String error) {
-		this.error = error;
-	}
-
 	public Applicant(java.lang.String highest_education,
 			java.lang.String field, java.lang.Integer work_exp,
-			java.lang.String pref_job, java.lang.Integer zip_code,
-			java.lang.String error) {
+			java.lang.String pref_job, java.lang.Integer zip_code) {
 		this.highest_education = highest_education;
 		this.field = field;
 		this.work_exp = work_exp;
 		this.pref_job = pref_job;
 		this.zip_code = zip_code;
-		this.error = error;
 	}
 
 }
